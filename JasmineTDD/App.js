095107eae594fcd71607390f5a4761262e108065
sankalpa <sab@exilesoft.com>
2014-05-12 11:40:54 +0530

    Initial

---
+++ JasmineTDD/App.js
@@ -0,0 +1,24 @@
+﻿function helloWorld() {
+	return "Hello World";
+};
+
+var app = angular.module("angularModule",[]);
+app.controller("angularController", function ($scope,$http) {
+	$scope.message = "Hello World";
+
+	$http.get("/userName").success(function(data) {
+		$scope.userName = data;
+	});
+
+	$scope.getAddress = function () {
+		$http.get("/address").success(function (data) {
+			$scope.Address = data;
+		});
+	};
+
+	$scope.saveUser = function(user) {
+		$http.post("/addUser", user).success(function() {
+
+		});
+	};
+});
---
+++ JasmineTDD/AppTest.js
@@ -0,0 +1,52 @@
+﻿
+/// <reference path="Scripts/jasmine/jasmine.js" />
+/// <reference path="Scripts/angular.js" />
+/// <reference path="Scripts/angular-mocks.js" />
+/// <reference path="App.js" />
+
+describe("Test Hello world", function () {
+
+	it("should return hello world", function() {
+		expect(helloWorld()).toEqual("Hello World");
+	});
+});
+
+describe("Test Angular Hello World", function () {
+	var scope;
+	var controller;
+	var httpBackend;
+	beforeEach(module("angularModule"));
+	
+	beforeEach(inject(function ($rootScope, $controller, $httpBackend) {
+		httpBackend = $httpBackend;
+		$httpBackend.expectGET("/userName").respond("Sankalpa");
+		scope = $rootScope.$new();
+		controller = $controller("angularController", { $scope: scope });
+		$httpBackend.flush();
+	}));
+	
+	it("Should return hello world", function() {
+		expect(scope.message).toEqual("Hello World");
+	});
+
+	it("Should get user name from server", function() {
+		expect(scope.userName).toEqual("Sankalpa");
+	});
+	
+	it("Should get user address from server", function () {
+		httpBackend.expectGET("/address").respond("Horana");
+		scope.getAddress();
+		httpBackend.flush();
+		
+		expect(scope.Address).toEqual("Horana");
+	});
+
+	it("Should save user", function () {
+		var user = {id:1, name:'test'};
+		httpBackend.expectPOST("/addUser", user).respond(200);
+		scope.saveUser(user);
+		httpBackend.flush();
+		
+		
+	});
+});
